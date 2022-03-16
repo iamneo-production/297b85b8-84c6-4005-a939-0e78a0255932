4ac897606506c666f448853ae50fe14a56c21543
wahid022 <rahmanwahid022@gmail.com>
2022-03-16 13:16:16 +0530

    changed the id of input tags in form of login and signup page

--- reactapp/src/component/Login.js
+++ reactapp/src/component/Login.js
@@ -2,23 +2,23 @@ import React from 'react';
 import img from './images/login_page.jpg';
 export default function Login() {
     return (
-        <div id='login-container'>
+        <div id='loginBox'>
             <img src={img}  alt="Music" id="login-image"/>
             <h1 className='my-3'>LOGIN</h1>
             <div className="mb-3">
                 <i className="fas fa-user fa-lg me-3 fa-fw pass-lock"></i> 
-                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email"/>
+                <input type="email" className="form-control" id="email" placeholder="Email"/>
             </div>
 
             <div className="mb-3">
                 <i className="fas fa-lock fa-lg me-3 fa-fw pass-lock"></i>
-                <input type="password" className="form-control" id="exampleFormControlInput2" placeholder="Password"/>
+                <input type="password" className="form-control" id="password" placeholder="Password"/>
             </div>
             
 
-            <button type="button" className="btn btn-secondary">LOGIN</button>
+            <button type="button" className="btn btn-secondary" id='submitButton'>LOGIN</button>
             <p>
-                <span>New to Bookstore? Click <a href='#'>here</a></span>
+                <span>New to Bookstore? Click <a href='#' id='signupLink'>here</a></span>
             </p>
         </div>
     );
