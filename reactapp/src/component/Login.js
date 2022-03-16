99329890ab1d74ae1166bf9c5ffbe123867c1717
wahid022 <rahmanwahid022@gmail.com>
2022-03-16 12:54:13 +0530

    added the font icons in login and signup page

--- reactapp/src/component/Login.js
+++ reactapp/src/component/Login.js
@@ -6,14 +6,17 @@ export default function Login() {
             <img src={img}  alt="Music" id="login-image"/>
             <h1 className='my-3'>LOGIN</h1>
             <div className="mb-3">
+                <i className="fas fa-user fa-lg me-3 fa-fw pass-lock"></i> 
                 <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email"/>
             </div>
 
             <div className="mb-3">
+                <i className="fas fa-lock fa-lg me-3 fa-fw pass-lock"></i>
                 <input type="password" className="form-control" id="exampleFormControlInput2" placeholder="Password"/>
             </div>
+            
 
-            <button type="button" class="btn btn-secondary">LOGIN</button>
+            <button type="button" className="btn btn-secondary">LOGIN</button>
             <p>
                 <span>New to Bookstore? Click <a href='#'>here</a></span>
             </p>
---
+++ reactapp/src/component/Sign_Up.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import img from './images/login_page.jpg';
+export default function Login() {
+    return (
+
+        <div id='sign-up-container'>
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" className="img-fluid" alt="Sample image"/>
+            {/* <img src={img}  alt="Music" id="sign-up-image"/> */}
+            <h1 className='my-3'>SIGN UP</h1>
+            <div className="mb-3">
+                 
+                <i class="fas fa-envelope fa-lg me-3 fa-fw pass-lock"></i>
+                <input type="email" className="form-control" id="email" placeholder="Enter Email"/>
+            </div>
+
+            <div className="mb-3">
+                <i className="fas fa-user fa-lg me-3 fa-fw pass-lock"></i>
+                <input type="text" className="form-control" id="username" placeholder="Enter Username"/>
+            </div>
+
+            <div className="mb-3">
+                <i class="fas fa-phone fa-lg me-3 fa-fw pass-lock"></i>
+                <input type="tel" className="form-control" id="mobilenumber" placeholder="Enter Mobile Number"/>
+            </div>
+
+            <div className="mb-3">
+                <i className="fas fa-lock fa-lg me-3 fa-fw pass-lock"></i>
+                <input type="password" className="form-control" id="password" placeholder="Enter Password"/>
+            </div>
+
+            <div className="mb-3">
+                <i class="fas fa-key fa-lg me-3 fa-fw pass-lock"></i>
+                <input type="password" className="form-control" id="confirmpassword" placeholder="Confirm Password"/>
+            </div>
+
+            <button type="button" id="submitButton" className="btn btn-secondary signup-submit-btn">SIGN UP</button>
+            <p>
+                <span id="signinLink">Already a member? Click <a href='#'>here</a></span>
+            </p>
+        </div>
+    );
+}
