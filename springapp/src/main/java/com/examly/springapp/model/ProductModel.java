b07ce55d8e0613b022f46ea070a1c9672c084395
Jeba Regan Raj <jebaregan21@gmail.com>
2022-03-17 11:28:00 +0530

    Initial model without annotations

--- springapp/src/main/java/com/examly/springapp/model/ProductModel.java
+++ springapp/src/main/java/com/examly/springapp/model/ProductModel.java
@@ -1,5 +1,61 @@
 package com.examly.springapp.model;
 
 public class ProductModel {
+	private String productId;
+	private String imageUrl;
+	private String productName;
+	private String price;
+	private String description;
+	private String quantity;
+	
+	public ProductModel() {}
 
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+	
 }
