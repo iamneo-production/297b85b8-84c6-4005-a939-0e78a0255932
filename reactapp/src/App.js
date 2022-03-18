b65da0ea48416229a28a1c5c09ae37b00c8ace28
wahid022 <rahmanwahid022@gmail.com>
2022-03-18 16:19:49 +0530

    form-validation is not working

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,36 +1,40 @@
-import React from 'react';
+// import React from 'react';
+import "./App.css";
 import Login from './component/Login';
 import Sign_Up from './component/Sign_Up';
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-function App() {
 
+function App() {
   return (
     <>
-    <Router>
-      <div className={"main container"}>
-          <Switch>
-          
-              <Route exact path="/">
-                <Login />
-              </Route>
-              <Route path="/login">
-                <Login />
-              </Route>
-              <Route path="/signup">
-                <Sign_Up />
-              </Route>
-          
-          </Switch>
-      </div>
-    </Router>
+
+
+      <Router>
+        <div className={"main container"}>
+            <Switch>
+            
+                <Route exact path="/">
+                  <Login />
+                </Route>
+                <Route path="/login">
+                  <Login />
+                </Route>
+                <Route path="/signup">
+                  <Sign_Up />
+                </Route>
+            
+            </Switch>
+        </div>
+      </Router>
     </>
   );
-
 }
+
 
 export default App;
--- reactapp/src/component/Login.js
+++ reactapp/src/component/Login.js
@@ -1,31 +1,91 @@
-import React from 'react';
+// import React from 'react';
 import img from './images/login_page.jpg';
-export default function Login() {
+import { useState, useEffect } from "react";
+
+function App() {
+
+        const initialValues = { email: "", password:"" };
+        const [formValues, setFormValues] = useState(initialValues);
+        const [formErrors, setFormErrors] = useState({});
+        const [isSubmit, setIsSubmit] = useState(false);
+
+        const handleChange = (e) => {
+            // console.log(e.target);
+            const { name, value } = e.target;
+            setFormValues({ ...formValues, [name]: value });
+            console.log(formValues);
+        };
+
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            setFormErrors(validate(formValues));
+            setIsSubmit(true);
+        };
+
+        useEffect(() => {
+            console.log(formErrors);
+            if (Object.keys(formErrors).length === 0 && isSubmit) {
+            // console.log(formValues);
+            }
+        }, [formErrors]);
+        const validate = (values) => {
+            const errors = {};
+            const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+            
+            if (!values.email) {
+                errors.email = "Email is required!";
+            } 
+            else if (!regex.test(values.email)) {
+                errors.email = "This is not a valid email format!";
+            }
+            if (!values.password) {
+                errors.password = "Password is required";
+            } else if (values.password.length < 4) {
+                errors.password = "Password must be more than 4 characters";
+            } else if (values.password.length > 10) {
+                errors.password = "Password cannot exceed more than 10 characters";
+            }
+            return errors;
+}
+
+
+//export default function Login() {
     return (
         <div id='loginBox'>
             <img src={img}  alt="Music" id="login-image"/>
             <h1 className='my-3'>LOGIN</h1>
-            <div className="mb-3">
-                <i className="fas fa-user fa-lg me-3 fa-fw pass-lock"></i> 
-                <input type="email" className="form-control" id="email" placeholder="Email"/>
-            </div>
 
-            <div className="mb-3">
-                <i className="fas fa-lock fa-lg me-3 fa-fw pass-lock"></i>
-                <input type="password" className="form-control" id="password" placeholder="Password"/>
-            </div>
-            
+            {Object.keys(formErrors).length === 0 && isSubmit ? (
+            <div className="ui message success">Signed in successfully</div>
+            ) : (
+                <pre>{JSON.stringify(formValues, undefined,1)}</pre>
+                )}
 
-            <button type="button" className="btn btn-secondary" id='submitButton'>LOGIN</button>
-            <p>
+            <form onSubmit={handleSubmit}>
 
+                    <div className="mb-3">
+                        <i className="fas fa-user fa-lg me-3 fa-fw pass-lock"></i> 
+                        <input type="email" className="form-control" id="email" placeholder="Email" value={formValues.email} onChange={handleChange}/>
+                    </div>
 
-                <span>New to E-Furniture Shopping? Click <a href='/signup'>here</a></span>
+                    <p>{formErrors.email}</p>
 
-            </p>
+                    <div className="mb-3">
+                        <i className="fas fa-lock fa-lg me-3 fa-fw pass-lock"></i>
+                        <input type="password" className="form-control" id="password" placeholder="Password"  value={formValues.password} onChange={handleChange}/>
+                    </div>
+                    
+                    <p>{formErrors.password}</p>
+
+                    <button type="button" className="btn btn-secondary" id='submitButton'>LOGIN</button>
+
+                    <p>
+                        <span>New to E-Furniture Shopping? Click <a href='/signup'>here</a></span>
+                    </p>
+                
+                </form>
         </div>
     );
 }
 
-// this is wahid 
-// this is really disgusting
+export default App;
