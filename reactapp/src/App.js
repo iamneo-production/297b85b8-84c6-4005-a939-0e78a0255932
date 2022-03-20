c3549e4eb98ae97b3f9c2dd345f62e35e54af2a9
gokulrajalp <gokulrajalp@gmail.com>
2022-03-20 22:58:38 +0530

    cleared the bug in nav_bar.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from './component/Navbar';
 import Login from './component/Login';
 import Sign_Up from './component/Sign_Up';
-import Alert from './component/Alert';
+// import Alert from './component/Alert';
 import Cart from './component/cart_page';
 import Order from './component/order_page';
 import { useState} from "react";
@@ -11,39 +11,39 @@ import Home from './component/home_page';
 
 import {
   BrowserRouter as Router,
-  Routes,
-  Route,
+  Switch,
+  Route
 } from "react-router-dom";
-
 
 function App() {
   
-        const [alert, setAlert] = useState(null);
+        // const [alert, setAlert] = useState(null);
         
-        const showAlert = (message, type)=>{
-            setAlert({
-              msg: message,
-              type: type
-            })
-            setTimeout(() => {
-                setAlert(null);
-            }, 1500);
-        }
+        // const showAlert = (message, type)=>{
+        //     setAlert({
+        //       msg: message,
+        //       type: type
+        //     })
+        //     setTimeout(() => {
+        //         setAlert(null);
+        //     }, 1500);
+        // }
 
         // showAlert("Light mode has been enabled", "success");
 
         return (
           <>
               <Router>
-              <Navbar title="E-Furniture" aboutText1="Cart" aboutText2="My order" aboutText3="Logout"/>
+              {/* <Navbar title="E-Furniture" aboutText1="Cart" aboutText2="My order" aboutText3="Logout"/> */}
 
-                <Alert alert={alert}/>
+                {/* <Alert alert={alert}/> */}
 
                 <div className={"main container"}>
                     <Switch>
                     
                         <Route exact path="/">
-                          <Login showAlert={showAlert} />
+                          
+                          <Login />
                         </Route>
                         <Route path="/login">
                           <Login />
@@ -53,14 +53,18 @@ function App() {
                         </Route>
 
                         <Route  path="/home">
+
+                        <Navbar title="E-Furniture" aboutText1="Cart" aboutText2="My order" aboutText3="Logout"/>
                           <Home heading="WELCOME HOME"/>
                         </Route>
 
                         <Route path="/cart">
+                        <Navbar title="E-Furniture" aboutText1="Cart" aboutText2="My order" aboutText3="Logout"/>
                           <Cart heading="WELCOME TO CART"/>
                         </Route>
 
                         <Route path="/orders">
+                        <Navbar title="E-Furniture" aboutText1="Cart" aboutText2="My order" aboutText3="Logout"/>
                           <Order heading="WELCOME TO MY ORDERS"/>
                         </Route>
                     
